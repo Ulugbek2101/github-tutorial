1edcbb2d8db0c77867771253e35bd7c8ac00a941
Ulugbek2101 <ulugbekaminboyev44@gmail.com>
2023-01-19 23:48:44 +0500

    if else and switch case haqida

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const numberOfSeries = +prompt("Nechta serial ko'rdingiz", "");
+/* const numberOfSeries = +prompt("Nechta serial ko'rdingiz", "");
 
 const seriesDB = {
     count: numberOfSeries,
@@ -19,4 +19,32 @@ seriesDB.series[a]=b;
 seriesDB.series[c]=d;
 
 
-console.log(numberOfSeries);
+console.log(numberOfSeries); */
+
+// const age = +prompt("How old are you?", "");
+
+// if (age>25) {
+//     console.log("Horror films");
+// } else if(age>18 && age<25) {
+//     console.log("Boyevik films");
+// } else {
+//     console.log("Carton");
+// }
+
+// (age>25) ? console.log("Horror films") : console.log("Carton")
+
+const color = "green";
+
+switch(color){
+    case "red":
+        console.log("Stop");
+        break;
+    case "green":
+        console.log("GO");
+        break;
+    case "yellow":
+        console.log("Slowly");
+        break;
+    default:
+        console.log("Traffic jam");
+}
